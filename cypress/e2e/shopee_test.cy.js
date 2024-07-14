3090e9e174d65eada3d1cb3e38ee4c0120e4a19e
Varaikorn <ext.varaikorn.cha@truedigital.com>
2024-07-14 20:43:00 +0700

    Test

--- cypress/e2e/shopee_test.cy.js
+++ cypress/e2e/shopee_test.cy.js
@@ -2,56 +2,39 @@ const getRandomDelay = () => Math.floor(Math.random() * 3000) + 1000;
 
 describe.only('Shopee Tests', () => {
   before(() => {
-    cy.visit('https://www.google.com');
+    cy.visit('/');
 
-    // Perform Google search for Shopee
-    cy.get('textarea[name="q"]').type('Shopee{enter}');
-    cy.wait(getRandomDelay());
-
-    // Click the div containing "Shopee Thailand" text
-    cy.contains('div', 'Shopee Thailand').click();
-    cy.wait(getRandomDelay());
-
-    // Handle the navigation to Shopee
-    cy.origin('https://shopee.co.th', () => {
-      // Check if the modal appears and handle it
-      cy.get('body').then(($body) => {
-        if ($body.find('#modal').length > 0) {
-          cy.wait(getRandomDelay()).get('#modal').within(() => {
-            cy.get('button.shopee-button-outline.shopee-button-outline--primary-reverse').contains('English').click();
-          });
-        }
-      });
+    // Check if the modal appears
+    cy.get('body').then(($body) => {
+      if ($body.find('#modal').length > 0) {
+        cy.wait(getRandomDelay()).get('#modal').within(() => {
+          cy.get('button.shopee-button-outline.shopee-button-outline--primary-reverse').contains('English').click();
+        });
+      }
     });
   });
 
   it('ScenarioA: Enter Shopee website, landing on home page with EN lang', () => {
-    cy.origin('https://shopee.co.th', () => {
-      cy.wait(getRandomDelay());
-      cy.get('input.Z7tNyT[name="loginKey"]').type('varaikornchankaipon@gmail.com', { delay: 200 }); // Slower typing delay
-      cy.wait(getRandomDelay());
-      cy.get('input.Z7tNyT[name="password"]').type('Aom@KMUTT555', { delay: 300 }); // Slower typing delay
-      cy.wait(getRandomDelay());
-      cy.get('button.vvOL40.iesrPs.AsFRg8.qCI4rz.ZKayWA.AnY7KS').should('not.be.disabled').click();
+    cy.wait(getRandomDelay());
+    cy.get('input.Z7tNyT[name="loginKey"]').type('varaikornchankaipon@gmail.com', { delay: 200 }); // Slower typing delay
+    cy.wait(getRandomDelay());
+    cy.get('input.Z7tNyT[name="password"]').type('Aom@KMUTT555', { delay: 300 }); // Slower typing delay
+    cy.wait(getRandomDelay());
+    cy.get('button.vvOL40.iesrPs.AsFRg8.qCI4rz.ZKayWA.AnY7KS').should('not.be.disabled').click();
 
-      cy.wait(getRandomDelay());
-      cy.get('.ZVb1Bo').should('contain', 'English');
-    });
+    cy.wait(getRandomDelay());
+    cy.get('.ZVb1Bo').should('contain', 'English');
   });
 
-  it('ScenarioB: Search for keywords “baby toys”', () => {
-    cy.origin('https://shopee.co.th', () => {
-      cy.wait(getRandomDelay());
-      cy.get('input.shopee-searchbar-input__input').type('baby toys', { delay: 200 }); // Slower typing delay
-      cy.wait(getRandomDelay());
-      cy.get('button.shopee-searchbar-input__submit').click();
-    });
+  it('ScenarioB : Search for keywords “ baby toys “', () => {
+    cy.wait(getRandomDelay());
+    cy.get('input.shopee-searchbar-input__input').type('baby toys', { delay: 200 }); // Slower typing delay
+    cy.wait(getRandomDelay());
+    cy.get('button.shopee-searchbar-input__submit').click();
   });
 
-  it('ScenarioC: Website display search result as a list', () => {
-    cy.origin('https://shopee.co.th', () => {
-      cy.wait(getRandomDelay());
-      cy.get('.shopee-search-item-result__items').should('exist');
-    });
+  it('ScenarioB : Website display search result as a list', () => {
+    cy.wait(getRandomDelay());
+    cy.get('.shopee-search-item-result__items').should('exist');
   });
 });
